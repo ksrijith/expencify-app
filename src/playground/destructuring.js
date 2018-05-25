cba023f8f288c06761bedff70f9c9f636de3a859
Srijith Kartha <ksrijith@gmail.com>
2018-05-25 06:18:25 +0000

    ES6 Array Destructuring challenge

--- src/playground/destructuring.js
+++ src/playground/destructuring.js
@@ -35,3 +35,8 @@ const [ , city, state ] = address;
 
 console.log(`You are in ${ city } ${ state }`);
 
+const item = ['Coffee (hot)', '$2.00', '$2.50', '$2.75'];
+
+const [name, , medium] = item;
+
+console.log(`A medium ${ name } costs ${ medium }`);
